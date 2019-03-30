917477c865b9a8b94755ece38c6a9ae5086a7f8d
XuanyiLi <xuanyili@mail.nankai.edu.cn>
2019-03-30 22:38:24 +0800

    clear wrong code to fix bug , wait for the right code
    clear wrong code to fix bug , wait for the right code

--- models/StereoNet8Xmulti.py
+++ models/StereoNet8Xmulti.py
@@ -53,37 +53,6 @@ class BasicBlock(nn.Module):
         out = x + out
         return out
 
-class FeatureExtraction(nn.Module):
-    def __init__(self, k):
-        super().__init__()
-        self.k = k
-        self.downsample = nn.ModuleList()
-        in_channel = 3
-        out_channel = 32
-        for _ in range(k):
-            self.downsample.append(
-                nn.Conv2d(
-                    in_channel,
-                    out_channel,
-                    kernel_size=5,
-                    stride=2,
-                    padding=2))
-            in_channel = out_channel
-            out_channel = 32
-        self.residual_blocks = nn.ModuleList()
-        for _ in range(6):
-            self.residual_blocks.append(
-                BasicBlock(
-                    32, 32, stride=1, downsample=None, pad=1, dilation=1))
-        self.conv_alone = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
-    def forward(self, rgb_img):
-        output = rgb_img
-        for i in range(self.k):
-            output = self.downsample[i](output)
-        for block in self.residual_blocks:
-            output = block(output)
-        return self.conv_alone(output)
-
 
 
 class disparityregression(nn.Module):
@@ -96,29 +65,6 @@ class disparityregression(nn.Module):
         disp = self.disp.repeat(x.size()[0], 1, x.size()[2], x.size()[3])
         out = torch.sum(x * disp, 1)
         return out
-
-if __name__ == '__main__':
-    model = StereoNet(k=3, r=3).cuda()
-    # model.eval()
-    import time
-    import datetime
-    import torch
-    
-    input = torch.FloatTensor(1,3,540,960).zero_().cuda()
-    # input = torch.half(1,3,540,960).zero_().cuda()
-
-    
-    for i in range(100):
-    # pass
-        out = model(input, input)
-        # print(len(out))
-    start = datetime.datetime.now()
-    for i in range(100):
-        # pass
-        out = model(input, input)
-
-    end = datetime.datetime.now()
-    print((end-start).total_seconds())
 
 
 
